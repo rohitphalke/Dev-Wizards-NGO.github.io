0be512cb5b7785b003422e21af4c087165e9103e
Virinchi26 <virinchishettigar26@gmail.com>
2023-07-21 17:59:41 +0530

    Changed Navbar and imported css/js from bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,79 @@
 import React from 'react'
-
 const Navbar = () => {
   return (
-    <div>
-      I am Navbar
-    </div>
-  )
+    <nav
+      className={"navbar navbar-expand-lg navbar-light bg-light"}
+    >
+      <a className="navbar-brand">NGO</a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active mx-3">
+            <a className="nav-a" href="/">
+              Home
+            </a>
+          </li>
+          <li className="nav-item mx-3">
+            <a className="nav-a" href="/about">
+              About
+            </a>
+          </li>
+          <li className="nav-item mx-3">
+            <a className="nav-a" href="/contact">
+              Contact Us
+            </a>
+          </li>
+          <li className="nav-item mx-3">
+            <a className="nav-a" href="/donate">
+              Donate
+            </a>
+          </li>
+          <li className="nav-item mx-3">
+            <a className="nav-a" href="/events">
+              Events
+            </a>
+          </li>
+          <li className="nav-item mx-3">
+            <a className="nav-a" href="/gallery">
+              Gallery
+            </a>
+          </li>
+          <li className="nav-item mx-3">
+            <a className="nav-a" href="/profile">
+              Profile
+            </a>
+          </li>
+        </ul>
+        {/* <form className="form-inline my-2 my-lg-0">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button
+            className="btn btn-primary my-2 my-sm-0"
+            type="submit"
+          >
+            Search
+          </button>
+        </form> */}
+      </div>
+
+    </nav>
+  );
 }
+
 
 export default Navbar
