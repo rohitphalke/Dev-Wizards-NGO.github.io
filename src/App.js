5d20ae04480d7f59bcc480b1921ed4f43b138369
rohit <rohit.phalke@spit.ac.in>
2023-07-21 10:58:49 +0530

    Added signup/signin page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import './rohit.css';
+import Login from './components/Login'
+import Navbar from './components/Navbar'
+import Gallery from './components/Gallery'
+import Footer from './components/Footer'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+          <Navbar/>
+          <Routes>
+            <Route path="/" element={<Gallery />} />
+            <Route path="/home" element={<Footer />} />
+            <Route path="/signup_login" element={<Login />} />
+          </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Gallery = () => {
   return (
     <div>
-      
+      I am gallery
     </div>
   )
 }
