42b6c8291f352a348ab9f2f6352ef4f32ec09073
adwaitp <adwait.purao@spit.ac.in>
2023-07-21 15:27:07 +0530

    Commit after merge

--- src/components/Events/EventDate.js
+++ src/components/Events/EventDate.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 import "./EventDate.css";
@@ -16,22 +15,3 @@ const EventDate = (props) => {
 };
 
 export default EventDate;
-=======
-import React from "react";
-
-import "./EventDate.css";
-
-const EventDate = (props) => {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
-
-  return (
-    <h4 className="EventDate">
-      <span>{month}  {day}  {year}</span>
-    </h4>
-  );
-};
-
-export default EventDate;
->>>>>>> cfaf63c60cdb519efbb7c39be5f51e02798458f2
