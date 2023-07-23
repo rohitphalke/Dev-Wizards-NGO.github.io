dd4ed863d96f59638202c0fb1a52e4a56447fa19
Virinchi26 <virinchishettigar26@gmail.com>
2023-07-23 19:26:27 +0530

    changing about

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      I am About
+    <>
+        <div class="my-5 px-4">
+        <h2 class="fw-bold h-font text-center">About Us</h2>
+        <div class="h-line bg-dark"></div>
+        <p class="text-center mt-3 text-dark">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi soluta
+            voluptatem cupiditate incidunt
+            totam,<br/> tenetur quis fuga nulla suscipit ea dicta nemo perferendis debitis expedita reiciendis molestias
+            delectus officiis temporibus.</p>
     </div>
+    </>
   )
 }
 
