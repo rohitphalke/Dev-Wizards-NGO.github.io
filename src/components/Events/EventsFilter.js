e3158c497cb2817ec9f14edf0061190cf807a4ce
rohit <rohit.phalke@spit.ac.in>
2023-07-23 17:19:13 +0530

    Events page

--- src/components/Events/EventsFilter.js
+++ src/components/Events/EventsFilter.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./EventsFilter.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 const EventsFilter = (props) => {
   const dropdownChangeHandler = (event) => {
